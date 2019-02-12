03e76a8d930bc54c8eb22c84a1854d80503b1f32
Patrick Przystolik <mtcllf@gmail.com>
2019-02-12 19:19:53 +0100

    addition, subtr, multipl, division unordered

--- src/tokenizer/tokenizer.spec.ts
+++ src/tokenizer/tokenizer.spec.ts
@@ -1,39 +1,59 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { Tokenizer } from './tokenizer';
+import { Tokenizer, Interpreter } from './tokenizer';
 
 let tokenizer: Tokenizer;
-
+let interpreter: Interpreter;
 describe('Tokenizer', () => {
 
   it('Single Digit: 3 + 7', () => {
     tokenizer = new Tokenizer('3 + 7');
-    const result = tokenizer.expr();
+    interpreter = new Interpreter(tokenizer);
+    const result = interpreter.expr();
     expect(result).to.equal(10);
   });
 
   it('Multiple Digit: 23 + 77', () => {
     tokenizer = new Tokenizer('23 + 77');
-    const result = tokenizer.expr();
+    interpreter = new Interpreter(tokenizer);
+    const result = interpreter.expr();
     expect(result).to.equal(100);
   });
 
   it('Multiple Digit: 15 - 90', () => {
     tokenizer = new Tokenizer('15 - 90');
-    const result = tokenizer.expr();
+    interpreter = new Interpreter(tokenizer);
+    const result = interpreter.expr();
     expect(result).to.equal(-75);
   });
 
   it('Multiple Signs: 7 - 3 + 2 - 1', () => {
     tokenizer = new Tokenizer('7 - 3 + 2 - 1');
-    const result = tokenizer.expr();
+    interpreter = new Interpreter(tokenizer);
+    const result = interpreter.expr();
     expect(result).to.equal(5);
   });
 
   it('Multiple Signs: 10 + 1 + 2 - 3 + 4 + 6 - 15', () => {
     tokenizer = new Tokenizer('10 + 1 + 2 - 3 + 4 + 6 - 15');
-    const result = tokenizer.expr();
+    interpreter = new Interpreter(tokenizer);
+    const result = interpreter.expr();
     expect(result).to.equal(5);
   });
+
+  it('MUL and DIV: 3 * 7', () => {
+    tokenizer = new Tokenizer('3 * 7');
+    interpreter = new Interpreter(tokenizer);
+    const result = interpreter.expr();
+    expect(result).to.equal(21);
+  });
+
+  it('MUL and DIV multiple: 3 * 7 / 2', () => {
+    tokenizer = new Tokenizer('3 * 7 / 2');
+    interpreter = new Interpreter(tokenizer);
+    const result = interpreter.expr();
+    expect(result).to.equal(10.5);
+  });
+
 });
--- src/tokenizer/tokenizer.ts
+++ src/tokenizer/tokenizer.ts
@@ -1,19 +1,25 @@
+
+
 export interface token {
   token: TOKENS,
   value: string | number,
 }
 
 export enum TOKENS {
-  NONE,
-  PLUS,
-  MINUS,
-  EOF,
-  INTEGER
+  NONE = 'NONE',
+  PLUS = '+',
+  MINUS = '-',
+  MUL = '*',
+  DIV = '/',
+  EOF = 'EOF',
+  INTEGER = 'INTEGER'
 }
 
 export enum SYMBOLS {
   PLUS = '+',
   MINUS = '-',
+  MUL = '*',
+  DIV = '/',
   NONE = '',
 }
 
@@ -58,7 +64,7 @@ export class Tokenizer {
     return +result;
   }
 
-  protected getNextToken(): token {
+  public getNextToken(): token {
     while (this.currentChar !== SYMBOLS.NONE) {
       if (this.isSpace(this.currentChar)) {
         this.skipWhitespace();
@@ -75,38 +81,63 @@ export class Tokenizer {
         this.advance();
         return { token: TOKENS.MINUS, value: SYMBOLS.MINUS };
       }
+      if (this.currentChar === SYMBOLS.MUL) {
+        this.advance();
+        return { token: TOKENS.MUL, value: SYMBOLS.MUL };
+      }
+      if (this.currentChar === SYMBOLS.DIV) {
+        this.advance();
+        return { token: TOKENS.DIV, value: SYMBOLS.DIV };
+      }
       throw new Error('Parsing Error');
     }
     return {token: TOKENS.EOF, value: null};
   }
 
-  protected eat(token: TOKENS): void {
-    if (this.currentToken.token === token) {
-      this.currentToken = this.getNextToken();
-      return;
-    }
-    throw new Error('Parsing Error, expected: ' + token + ' got: ' + this.currentToken);
+}
+
+export class Interpreter {
+  tokenizer: Tokenizer;
+  currentToken: token;
+
+  constructor(tokenizer: Tokenizer) {
+    this.tokenizer = tokenizer;
+    this.currentToken = this.tokenizer.getNextToken();
   }
 
-  protected term(): string | number {
+  protected eat(token: TOKENS): void {
+    if (this.currentToken.token === token) {
+      this.currentToken = this.tokenizer.getNextToken();
+      return;
+    }
+    throw new Error('Parsing Error, expected: ' + token + ' got: ' + this.currentToken.token);
+  }
+
+  protected factor(): number {
     const token = this.currentToken;
     this.eat(TOKENS.INTEGER);
-    return token.value;
+    return <number>token.value;
   }
 
   public expr() {
-    this.currentToken = this.getNextToken();
-    let result = this.term();
-    while (this.currentToken.token === TOKENS.PLUS || this.currentToken.token === TOKENS.MINUS) {
+    let result = this.factor();
+    while (this.currentToken.token === TOKENS.MUL || this.currentToken.token === TOKENS.DIV || this.currentToken.token === TOKENS.PLUS || this.currentToken.token === TOKENS.MINUS) {
       const token = this.currentToken;
-      if (token.token === TOKENS.PLUS) {
+      if (token.token === TOKENS.MUL) {
+        this.eat(TOKENS.MUL);
+        result = result * this.factor();
+      } else if (token.token === TOKENS.DIV) {
+        this.eat(TOKENS.DIV);
+        result = result / this.factor();
+      } else if (token.token === TOKENS.PLUS) {
         this.eat(TOKENS.PLUS);
-        result = <number>result + <number>this.term();
+        result = <number>result + <number>this.factor();
       } else if (token.token === TOKENS.MINUS) {
         this.eat(TOKENS.MINUS);
-        result = <number>result - <number>this.term();
+        result = <number>result - <number>this.factor();
       }
     }
     return result;
   }
+
 }
